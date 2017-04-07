432eab9dd5d812912426cdd8f7237fbda5fa5fa7
Brianvoe <brian@webiswhatido.com>
2017-04-07 00:01:27 -0500

    highlights - added github theme

--- src/docs/index.js
+++ src/docs/index.js
@@ -4,10 +4,37 @@ import VueRouter from 'vue-router'
 import app from 'docs/app.vue'
 import routerList from 'docs/router.js'
 
+import hljs from 'highlightjs'
 import './google-analytics.js'
 
-import 'scss/docs.scss'
+import 'scss/index.scss'
 import 'slim-select/index.scss'
+
+Vue.directive('highlightjs', {
+  deep: true,
+  bind: function (el, binding) {
+    // on first bind, highlight all targets
+    let targets = el.querySelectorAll('code')
+    targets.forEach((target) => {
+      // if a value is directly assigned to the directive, use this
+      // instead of the element content.
+      if (binding.value) {
+        target.innerHTML = binding.value
+      }
+      hljs.highlightBlock(target)
+    })
+  },
+  componentUpdated: function (el, binding) {
+    // after an update, re-fill the content and then highlight
+    let targets = el.querySelectorAll('code')
+    targets.forEach((target) => {
+      if (binding.value) {
+        target.innerHTML = binding.value
+        hljs.highlightBlock(target)
+      }
+    })
+  }
+})
 
 // Router setup
 Vue.use(VueRouter)
--- src/docs/pages/home.vue
+++ src/docs/pages/home.vue
@@ -1,8 +1,45 @@
 <script>
+  import SlimSelect from 'slim-select/index.ts'
+
+  export default {
+    mounted () {
+      /* eslint-disable no-new */
+      new SlimSelect({
+        select: '#slim-select'
+      })
+    }
+  }
 </script>
+
+<style lang="scss">
+  #home-content {
+    .install-code, #slim-select, .example-code {
+      width: 250px;
+      margin: 0 auto;
+    }
+  }
+</style>
 
 <template>
   <div id="home-content">
-    Home page
+    <div class="install-code">
+      <pre v-highlightjs><code class="bash">
+      yarn add slim-select
+      </code></pre>
+    </div>
+
+    <select id="slim-select">
+      <option value="best">Best</option>
+      <option value="select">Select</option>
+      <option value="ever">Ever</option>
+    </select>
+
+    <div class="example-code">
+      <pre v-highlightjs><code class="javascript">
+      new SlimSelect({
+        select: '#slim-select'
+      })
+      </code></pre>
+    </div>
   </div>
 </template>
