e61728d04d7d73f356f3bc0244cee109cf771786
Aurimas.Laureckis <aurimas.laureckis@bentley.com>
2020-04-09 15:16:44 +0300

    updated documentation

--- src/docs/pages/options.vue
+++ src/docs/pages/options.vue
@@ -147,6 +147,23 @@ export default Vue.extend({
       }
     })
 
+    const mandatorySelect = new SlimSelect({
+      select: '#selectMultiMandatory'
+    })
+
+    const mandatoryData = [
+      { value: 'A', text: 'A', mandatory: true },
+      { value: 'B', text: 'B' },
+      { value: 'C', text: 'C' }
+    ]
+
+    mandatorySelect.setData(mandatoryData)
+    mandatorySelect.set(['A', 'C'])
+
+    new SlimSelect({
+      select: '#selectMultiMandatory2'
+    })
+
     new SlimSelect({
       select: '#selectMultiLimit',
       limit: 2
@@ -799,6 +816,66 @@ export default Vue.extend({
             &lt;option value="value1"&gt;Value 1&lt;/option&gt;
             &lt;option value="value2"&gt;Value 2&lt;/option&gt;
             &lt;option value="value3"&gt;Value 3&lt;/option&gt;
+          &lt;/select&gt;
+        </code>
+      </pre>
+    </div>
+
+    <div class="content">
+      <h2 class="header">mandatory</h2>
+      <p>When using multi select you can set a mandatory on the option to prevent capability to deselect particular option. Note options with mandatory flag is not selected by default, you need select them yourselfs.</p>
+
+      <div class="set-content">
+        <select id="selectMultiMandatory" multiple></select>
+      </div>
+
+      <pre>
+        <code class="language-javascript">
+            const slim = new SlimSelect({
+                select: '#selectMultiMandatory'
+            });
+
+            const data = [
+                { value: 'A', text: 'A', mandatory: true },
+                { value: 'B', text: 'B' },
+                { value: 'C', text: 'C' }
+            ]
+
+            slim.setData(data)
+            slim.set(["A", "C"])
+        </code>
+      </pre>
+
+      <pre>
+        <code class="language-html">
+          &lt;select id="selectMultiMandatory" multiple&gt;
+          &lt;/select&gt;
+        </code>
+      </pre>
+
+      <p>Or</p>
+      <div class="set-content">
+        <select id="selectMultiMandatory2" multiple>
+          <option value="A" data-mandatory="true" selected>A</option>
+          <option value="B" selected>B</option>
+          <option value="C">C</option>
+        </select>
+      </div>
+
+      <pre>
+        <code class="language-javascript">
+            const slim = new SlimSelect({
+                select: '#selectMultiMandatory2'
+            });
+        </code>
+      </pre>
+
+      <pre>
+        <code class="language-html">
+          &lt;select id="selectMultiMandatory" multiple&gt;
+            &lt;option value="A" data-mandatory="true" selected&gt;A&lt;/option&gt;
+            &lt;option value="B" selected&gt;B&lt;/option&gt;
+            &lt;option value="C"&gt;C&lt;/option&gt;
           &lt;/select&gt;
         </code>
       </pre>
