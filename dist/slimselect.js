e3cadcfb0015605ef0b390d9022b9cf81f3dc3b5
brianvoe <brian@webiswhatido.com>
2020-04-26 00:05:43 -0500

    output - dist docs update

--- dist/slimselect.js
+++ dist/slimselect.js
@@ -200,6 +200,13 @@ function highlight(str, search, className) {
     return completedString;
 }
 exports.highlight = highlight;
+function kebabCase(str) {
+    var result = str.replace(/[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g, function (match) { return '-' + match.toLowerCase(); });
+    return (str[0] === str[0].toUpperCase())
+        ? result.substring(1)
+        : result;
+}
+exports.kebabCase = kebabCase;
 (function () {
     var w = window;
     if (typeof w.CustomEvent === 'function') {
@@ -1105,6 +1112,7 @@ exports.Config = Config;
 "use strict";
 
 exports.__esModule = true;
+var helper_1 = __webpack_require__(0);
 var Select = (function () {
     function Select(info) {
         this.triggerMutationObserver = true;
@@ -1216,10 +1224,13 @@ var Select = (function () {
     };
     Select.prototype.createOption = function (info) {
         var optionEl = document.createElement('option');
-        optionEl.value = info.value || info.text;
+        optionEl.value = info.value !== '' ? info.value : info.text;
         optionEl.innerHTML = info.innerHTML || info.text;
         if (info.selected) {
             optionEl.selected = info.selected;
+        }
+        if (info.display === false) {
+            optionEl.style.display = 'none';
         }
         if (info.disabled) {
             optionEl.disabled = true;
@@ -1237,7 +1248,7 @@ var Select = (function () {
         }
         if (info.data && typeof info.data === 'object') {
             Object.keys(info.data).forEach(function (key) {
-                optionEl.setAttribute('data-' + key, info.data[key]);
+                optionEl.setAttribute('data-' + helper_1.kebabCase(key), info.data[key]);
             });
         }
         return optionEl;
@@ -1476,43 +1487,39 @@ var Slim = (function () {
         text.classList.add(this.main.config.valueText);
         text.innerHTML = (optionObj.innerHTML && this.main.config.valuesUseText !== true ? optionObj.innerHTML : optionObj.text);
         value.appendChild(text);
-        if (optionObj.mandatory) {
-            return value;
-        }
-        var deleteSpan = document.createElement('span');
-        deleteSpan.classList.add(this.main.config.valueDelete);
-        deleteSpan.innerHTML = this.main.config.deselectLabel;
-        deleteSpan.onclick = function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            var shouldUpdate = false;
-            if (!_this.main.config.isEnabled) {
-                return;
-            }
-            if (!_this.main.beforeOnChange) {
-                shouldUpdate = true;
-            }
-            if (_this.main.beforeOnChange) {
-                var selected = _this.main.data.getSelected();
-                var currentValues = JSON.parse(JSON.stringify(selected));
-                for (var i = 0; i < currentValues.length; i++) {
-                    if (currentValues[i].id === optionObj.id) {
-                        currentValues.splice(i, 1);
-                    }
-                }
-                var beforeOnchange = _this.main.beforeOnChange(currentValues);
-                if (beforeOnchange !== false) {
+        if (!optionObj.mandatory) {
+            var deleteSpan = document.createElement('span');
+            deleteSpan.classList.add(this.main.config.valueDelete);
+            deleteSpan.innerHTML = this.main.config.deselectLabel;
+            deleteSpan.onclick = function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                var shouldUpdate = false;
+                if (!_this.main.beforeOnChange) {
                     shouldUpdate = true;
                 }
-            }
-            if (shouldUpdate) {
-                _this.main.data.removeFromSelected(optionObj.id, 'id');
-                _this.main.render();
-                _this.main.select.setValue();
-                _this.main.data.onDataChange();
-            }
-        };
-        value.appendChild(deleteSpan);
+                if (_this.main.beforeOnChange) {
+                    var selected = _this.main.data.getSelected();
+                    var currentValues = JSON.parse(JSON.stringify(selected));
+                    for (var i = 0; i < currentValues.length; i++) {
+                        if (currentValues[i].id === optionObj.id) {
+                            currentValues.splice(i, 1);
+                        }
+                    }
+                    var beforeOnchange = _this.main.beforeOnChange(currentValues);
+                    if (beforeOnchange !== false) {
+                        shouldUpdate = true;
+                    }
+                }
+                if (shouldUpdate) {
+                    _this.main.data.removeFromSelected(optionObj.id, 'id');
+                    _this.main.render();
+                    _this.main.select.setValue();
+                    _this.main.data.onDataChange();
+                }
+            };
+            value.appendChild(deleteSpan);
+        }
         return value;
     };
     Slim.prototype.contentDiv = function () {
