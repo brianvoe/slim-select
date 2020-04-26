d57a7ad0243b3d117ba2040c2f339ce183a82382
brianvoe <brian@webiswhatido.com>
2020-04-25 23:58:53 -0500

    helper - minor ts fix

--- src/slim-select/helper.ts
+++ src/slim-select/helper.ts
@@ -93,7 +93,7 @@ export function highlight(str: string, search: any, className: string) {
 export function kebabCase(str: string) {
   const result = str.replace(
     /[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g,
-    match => '-' + match.toLowerCase()
+    (match) => '-' + match.toLowerCase()
   )
   return (str[0] === str[0].toUpperCase())
     ? result.substring(1)
