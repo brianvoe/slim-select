9300d9fce7942713cb927d97770bfc6aae0c9669
brianvoe <brian@webiswhatido.com>
2022-10-15 18:30:43 -0500

    vite - updated build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [vue()]
 })
