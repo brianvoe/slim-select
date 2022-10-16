01ae6bbda167050b89f261f6c33049d4336aa938
brianvoe <brian@webiswhatido.com>
2022-10-16 16:16:51 -0500

    lint - format

--- src/slim-select/rollup.config.mjs
+++ src/slim-select/rollup.config.mjs
@@ -2,12 +2,14 @@ import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-    input: './index.ts',
-    output: [{
-        name: 'SlimSelect',
-        file: '../../dist/slimselect.umd.js',
-        format: 'umd',
-        sourcemap: true
-    }],
-    plugins: [typescript(), terser()]
+  input: './index.ts',
+  output: [
+    {
+      name: 'SlimSelect',
+      file: '../../dist/slimselect.umd.js',
+      format: 'umd',
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript(), terser()],
 }
