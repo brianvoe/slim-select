3f7cbabd6ba5d4ed9d641ae5121d1b801ddfa530
bvoelker <bvoelker@fluidpay.com>
2022-10-20 20:02:27 -0500

    jest - config update

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   testMatch: ['**/*.test.ts'],
   roots: ['./src/slim-select'],
 }
