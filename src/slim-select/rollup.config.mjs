0ba709afc628710ef4832a4608adcce0f6fc00c1
brianvoe <brian@webiswhatido.com>
2022-11-20 13:15:59 -0600

    rollup - added other output types

--- src/slim-select/rollup.config.mjs
+++ src/slim-select/rollup.config.mjs
@@ -6,6 +6,30 @@ export default {
   output: [
     {
       name: 'SlimSelect',
+      file: '../../dist/slimselect.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      name: 'SlimSelect',
+      file: '../../dist/slimselect.es.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      name: 'SlimSelect',
+      file: '../../dist/slimselect.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      name: 'SlimSelect',
+      file: '../../dist/slimselect.global.js',
+      format: 'iife',
+      sourcemap: true,
+    },
+    {
+      name: 'SlimSelect',
       file: '../../dist/slimselect.umd.js',
       format: 'umd',
       sourcemap: true,
