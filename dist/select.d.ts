68198bd1d67ff5185d207572424bec19c6050a68
brianvoe <brian@webiswhatido.com>
2022-11-20 13:16:09 -0600

    build - output update

--- dist/select.d.ts
+++ dist/select.d.ts
@@ -2,7 +2,8 @@ import { DataArray, Optgroup, Option } from './store';
 export default class Select {
     select: HTMLSelectElement;
     listen: boolean;
-    changeFunc?: (data: DataArray) => void;
+    onSelectChange?: (data: DataArray) => void;
+    onValueChange?: (value: string[]) => void;
     private observer;
     constructor(select: HTMLSelectElement);
     enable(): void;
@@ -10,18 +11,23 @@ export default class Select {
     hideUI(): void;
     showUI(): void;
     changeListen(on: boolean): void;
-    addChangeFunc(func: (data: DataArray) => void): void;
-    removeChangeFunc(): void;
-    setSelected(value: string[]): void;
+    addSelectChangeListener(func: (data: DataArray) => void): void;
+    removeSelectChangeListener(): void;
+    addValueChangeListener(func: (value: string[]) => void): void;
+    removeValueChangeListener(): void;
+    valueChange(ev: Event): any;
     private observeWrapper;
     private addObserver;
     private connectObserver;
     private disconnectObserver;
     getData(): DataArray;
     getDataFromOptgroup(optgroup: HTMLOptGroupElement): Optgroup;
+    getSelectedValues(): string[];
     getDataFromOption(option: HTMLOptionElement): Option;
+    setSelected(value: string[]): void;
     updateSelect(id?: string, style?: string, classes?: string[]): void;
     updateOptions(data: DataArray): void;
     createOptgroup(optgroup: Optgroup): HTMLOptGroupElement;
     createOption(info: Option): HTMLOptionElement;
+    destroy(): void;
 }
