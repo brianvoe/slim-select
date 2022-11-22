c6962870c12d7dce9819c228e77c99c433fa8aeb
bvoelker <bvoelker@fluidpay.com>
2022-11-22 15:19:07 -0600

    rollup - updated file outputs

--- src/slim-select/rollup.config.mjs
+++ src/slim-select/rollup.config.mjs
@@ -1,40 +1,115 @@
 import typescript from '@rollup/plugin-typescript'
-import { terser } from 'rollup-plugin-terser'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 
-export default {
-  input: './index.ts',
-  output: [
-    // Default umd
-    {
-      name: 'SlimSelect',
-      file: '../../dist/slimselect.js',
-      format: 'umd',
-      sourcemap: true,
-    },
-    {
-      name: 'SlimSelect',
-      file: '../../dist/slimselect.es.js',
-      format: 'es',
-      sourcemap: true,
-    },
-    {
-      name: 'SlimSelect',
+export default [
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  {
+    input: './index.ts',
+    output: {
       file: '../../dist/slimselect.cjs.js',
       format: 'cjs',
-      sourcemap: true,
+      exports: 'default',
     },
-    {
+    plugins: [
+      typescript(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+        babelrc: false,
+        presets: [
+          [
+            '@babel/env',
+            {
+              modules: false,
+              useBuiltIns: 'usage',
+              targets: 'maintained node versions',
+            },
+          ],
+        ],
+      }),
+    ],
+  },
+
+  // UMD build for browsers
+  {
+    input: './index.ts',
+    output: [
+      {
+        name: 'SlimSelect',
+        file: '../../dist/slimselect.js',
+        format: 'umd',
+      },
+      {
+        name: 'SlimSelect',
+        file: '../../dist/slimselect.umd.js',
+        format: 'umd',
+      },
+    ],
+    plugins: [
+      typescript(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+  },
+
+  // UMD build for browsers minified with terser
+  {
+    input: './index.ts',
+    output: {
+      name: 'SlimSelect',
+      file: '../../dist/slimselect.umd.min.js',
+      format: 'umd',
+    },
+    plugins: [
+      typescript(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      terser(),
+    ],
+  },
+
+  // ES module build for modern browsers
+  {
+    input: './index.ts',
+    output: {
+      file: '../../dist/slimselect.es.js',
+      format: 'es',
+    },
+    plugins: [
+      typescript(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+  },
+
+  // IIFE build for modern browsers
+  {
+    input: './index.ts',
+    output: {
       name: 'SlimSelect',
       file: '../../dist/slimselect.global.js',
       format: 'iife',
-      sourcemap: true,
     },
-    {
-      name: 'SlimSelect',
-      file: '../../dist/slimselect.umd.js',
-      format: 'umd',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript(), terser()],
-}
+    plugins: [
+      typescript(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
+  },
+]
