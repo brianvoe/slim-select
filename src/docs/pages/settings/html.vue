b9148d5b306c882168180912401bdefaf0863d4f
bvoelker <bvoelker@fluidpay.com>
2022-12-01 11:37:59 -0600

    docs - updated html usage

--- src/docs/pages/settings/html.vue
+++ src/docs/pages/settings/html.vue
@@ -7,7 +7,7 @@ export default defineComponent({
   name: 'Html',
   mounted() {
     new SlimSelect({
-      select: this.$refs.selectHtmlSingle as HTMLSelectElement,
+      select: this.$refs.htmlSingle as HTMLSelectElement,
       settings: {
         searchHighlight: true,
       },
@@ -18,38 +18,38 @@ export default defineComponent({
           value: 'bold text',
         },
         {
-          html: '<i>Slim Select you are awesome</i>',
-          text: 'Slim Select awesome',
-        },
-        {
           html: '<div style="border: solid 1px #666666;">Border</div>',
           text: 'Border',
           value: 'border',
+        },
+        {
+          html: '<i>Slim Select is awesome</i>',
+          text: 'Slim Select is awesome',
         },
       ],
     })
 
     new SlimSelect({
-      select: this.$refs.selectHtmlMulti as HTMLSelectElement,
+      select: this.$refs.htmlMulti as HTMLSelectElement,
       settings: {
         searchHighlight: true,
       },
-      data: [
-        {
-          html: '<b>Bold Text</b>',
-          text: 'Bold Text',
-          value: 'bold text',
-        },
-        {
-          html: '<i>Slim Select you are awesome</i>',
-          text: 'Slim Select awesome',
-        },
-        {
-          html: '<div style="border: solid 1px #666666;">Border</div>',
-          text: 'Border',
-          value: 'border',
-        },
-      ],
+      // data: [
+      //   {
+      //     html: '<b>Bold Text</b>',
+      //     text: 'Bold Text',
+      //     value: 'bold text',
+      //   },
+      //   {
+      //     html: '<div style="border: solid 1px #666666;">Border</div>',
+      //     text: 'Border',
+      //     value: 'border',
+      //   },
+      //   {
+      //     html: '<i>Slim Select is awesome</i>',
+      //     text: 'Slim Select is awesome',
+      //   },
+      // ],
     })
   },
 })
@@ -65,21 +65,36 @@ export default defineComponent({
     </p>
 
     <div class="row">
-      <select ref="selectHtmlSingle"></select>
-      <select ref="selectHtmlMulti" multiple></select>
+      <select ref="htmlSingle"></select>
+      <select ref="htmlMulti" multiple>
+        <option value="bold text" data-html="<b>Bold Text</b>">Bold Text</option>
+        <option value="border" data-html="<div style='border: solid 1px #666666;'>Border</div>">Border</option>
+        <option value="slim select" data-html="<i>Slim Select is awesome</i>">Slim Select is awesome</option>
+      </select>
     </div>
 
+    <h3>Via data</h3>
     <pre>
       <code class="language-javascript">
         var select = new SlimSelect({
           select: '#selectElement',
           data: [
-            {html: '&lt;img height="30" width="30" src="http://lorempixel.com/30/30" /&gt;&nbsp;Image', text: 'Image', value: 'image'},
             {html: '&lt;b&gt;Bold Text&lt;/b&gt;', text: 'Bold Text', value: 'bold text'},
             {html: '&lt;div style="border: solid 1px #666666;"&gt;Border&lt;/div&gt;', text: 'Border', value: 'border'},
-            {html: '&lt;i&gt;Slim Select you are awesome&lt;/i&gt;', text: 'Slim Select awesome'}
+            {html: '&lt;i&gt;Slim Select is awesome&lt;/i&gt;', text: 'Slim Select is awesome'}
           ]
         })
+      </code>
+    </pre>
+
+    <h3>Via data attribute</h3>
+    <pre>
+      <code class="language-html">
+        &lt;select id="selectElement"&gt;
+          &lt;option value="bold text" data-html="&lt;b&gt;Bold Text&lt;/b&gt;"&gt;Bold Text&lt;/option&gt;
+          &lt;option value="border" data-html="&lt;div style=&quot;border: solid 1px #666666;&quot;&gt;Border&lt;/div&gt;"&gt;Border&lt;/option&gt;
+          &lt;option value="slim select" data-html="&lt;i&gt;Slim Select is awesome&lt;/i&gt;"&gt;Slim Select awesome&lt;/option&gt;
+        &lt;/select&gt;
       </code>
     </pre>
   </div>
