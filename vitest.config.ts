3fae2499e7b1e355fe4d5eb63abfaf4b2d834603
brianvoe <brian@webiswhatido.com>
2025-10-21 10:57:53 -0500

    test - added @ alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
